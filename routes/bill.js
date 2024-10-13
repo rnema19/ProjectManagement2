3d95cd1f9d8d7be2d6813db835d2c17b3d5ba252
rnema19 <nemarishabh9@gmail.com>
2024-10-13 16:57:39 +0530

    added billing in ejs

--- routes/bill.js
+++ routes/bill.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express()
-// var Router = require('routes');
 var router = express.Router({mergeParams:true})
 const path = require('path');
 const bill = require('../model/billSchema')
 const project = require('../model/projectSchema')
-
-
-// app.set('views',path.join(__dirname,'../views'))
-// app.use(express.static(path.join(__dirname,'./public')))
 
 router.get('/',async(req,res)=>{
     const project_id = req.params.id
     console.log(`Displaying billing page for project id: ${project_id}`)
     res.render('billing',{project_id})
 })    
+
+router.get('/addbill',async(req,res)=>{
+    const project_id = req.params.id
+    // console.log(`Displaying billing page for project id: ${project_id}`)
+    res.render('addbill',{project_id})
+}) 
+
+// router.post('/',async(req,res)=>{
+//     const project_id = req.params.id
+//     console.log(`Displaying billing page for project id: ${project_id}`)
+
+//     res.redirect('billing',{project_id})
+// })    
 
 module.exports = router
 
